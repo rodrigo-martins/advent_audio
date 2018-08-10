95a2913209f8c2e04f151f0ffb0166f9256d778a
Rodrigo Siqueira <rodrigo.siqueira@visie.com.br>
2018-08-10 14:38:01 -0300

    Adionando Media

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,126 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+var myApp = angular.module('myApp', ['ngMaterial', 'ngMessages'])
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+myApp.filter('secondsToTime', function() {
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    function padTime(t) {
+        return t < 10 ? "0"+t : t;
     }
-};
 
-app.initialize();
+    return function(_seconds) {
+        if (typeof _seconds !== "number" || _seconds <= 0)
+            return "00:00";
+        var hours = Math.floor(_seconds / 3600),
+            minutes = Math.floor((_seconds % 3600) / 60),
+            seconds = Math.floor(_seconds % 60);
+
+        if(hours<0)    
+            return padTime(hours) + ":" + padTime(minutes) + ":" + padTime(seconds);
+        else
+            return padTime(minutes) + ":" + padTime(seconds);
+    };
+});
+
+myApp.controller('myCtrl', function ($scope) {
+
+    var media = null
+
+    $scope.status = 0
+    $scope.duration = 0
+    $scope.position = 0
+
+
+    $scope.playAudio = function (url) {
+
+        //if paused
+        if($scope.status == 3 && media.src == url){
+            media.play()
+            return;
+        }
+
+        //if another audio
+        if( media && media.src != url){
+            media.stop()
+            media.release()
+        }
+
+        if($scope.status == 0 || $scope.status == 4){
+            // Play the audio file at url
+            media = new Media(url,
+                // success callback
+                function() {
+                    console.log("playAudio():Audio Success");
+                },
+                // error callback
+                function(err) {
+                    console.log("playAudio():Audio Error: ",err);
+                },
+                function(e){
+                    $scope.status = e
+                    $scope.duration = media.getDuration()
+
+                    if(e == 2){
+                        var clock = setInterval(function(){
+                            media.getCurrentPosition(
+                                // success callback
+                                function (position) {
+                                    if (position > -1) {
+                                        $scope.position = position
+                                        $scope.$apply()
+                                    }
+                                },
+                                // error callback
+                                function (e) {
+                                    console.log("Error getting pos=" + e);
+                                }
+                            )
+    
+                        },1000)
+
+                        var counter = 0;
+                        var timerDur = setInterval(function() {
+                            counter += + 100;
+                            if (counter > 2000) {
+                                clearInterval(timerDur);
+                            }
+                            var duration = media.getDuration();
+                            if (duration > 0) {
+                                clearInterval(timerDur);
+                                $scope.duration = duration
+                                $scope.$apply()
+                            }
+                        }, 100);    
+
+                    }else{
+                        clearTimeout(clock)
+                    }
+
+                    if(e == 4){
+                        $scope.duration = 0
+                        $scope.position = 0
+                    }
+
+                    $scope.$apply()
+                }
+            );
+            // Play audio
+            media.play()
+            console.log(media)
+        }
+    }
+
+    $scope.pause = function (){
+        if(media){
+            media.pause()
+        }
+    }
+
+    $scope.stop = function (){
+        if(media){
+            media.stop()
+        }
+    }
+
+    $scope.seekTo = function(){
+        media.seekTo()
+    }
+})
